e419a161d5658b1bbdf66008a2090e4fa0f26635
Dylan Jack <79838810+DylanJackUni@users.noreply.github.com>
2021-03-30 00:24:35 +1100

    Moved View to Frontend Folder

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+//TODO create Frontend using react in the frontend Dir
+//then setup the concurency scripts to run front and backend
 const express = require('express');
-
 const app = express();
 
 app.use(express.json());
