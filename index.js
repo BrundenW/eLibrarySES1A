1b55f4448210661ef5af6ca2dd9c30831d32f385
Dylan Jack <79838810+DylanJackUni@users.noreply.github.com>
2021-03-30 14:50:22 +1100

    Start of the DB code

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 //TODO create Frontend using react in the frontend Dir
 //then setup the concurency scripts to run front and backend
 const express = require('express');
-const app = express();
+const lowDb = require("lowdb")
+const FileSync = require("lowdb/adapters/FileSync")
 
+const db = lowDb(new FileSync('db.json'))
+db.defaults({ notes: []}).write()
+
+const app = express();
 app.use(express.json());
+
+app.get('/notes', (req, res) => {
+  const data = db.get("notes").value()
+  return res.json(data)
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
