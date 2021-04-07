89f56d0e9900a215d646824539c4dcceb74e7cc8
Hussein Sultani <79577075+hussein-sultani@users.noreply.github.com>
2021-04-07 18:06:34 +1000

    Add files via upload
    my app.js

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+//const Blog = require('./models/blog');
+//const blogRoutes = require('./routes/blogRoutes');
+const { result } = require('lodash');
+
+// express app
+const app = express();
+
+// connection to mongodb
+const dbURI = 'mongodb+srv://eLibrary:eL123456@cluster0.tb5f9.mongodb.net/eLibraryServer?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://eLibrary:eL123456@cluster0.tb5f9.mongodb.net/eLibraryServer?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    client.close();
+});
+app.listen(3000);
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+app.post('/blogs', (req, res) => {
+    // console.log(req.body);
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then(result => {
+            res.redirect('/blogs');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs' });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+
+
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
