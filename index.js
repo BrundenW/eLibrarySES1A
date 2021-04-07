0e5c99a81680208519428055e9f05b1753c545b0
Dylan Jack <79838810+DylanJackUni@users.noreply.github.com>
2021-04-07 17:14:55 +1000

    Storage of data User

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ const lowDb = require("lowdb")
 const FileSync = require("lowdb/adapters/FileSync")
 
 const db = lowDb(new FileSync('db.json'))
-db.defaults({ notes: []}).write()
+db.defaults({ posts: []}).write()
 
 const app = express();
 app.use(express.json());
 
-app.get('/notes', (req, res) => {
-  const data = db.get("notes").value()
+app.get('/posts', (req, res) => {
+  const data = db.get("posts").value()
   return res.json(data)
+})
+
+app.post('/posts/new', (req, res) => {
+  db.get("posts")
+    .push(req.body)
+    .write()
+    console.log(req.body)
 })
 
 const PORT = process.env.PORT || 5000;
