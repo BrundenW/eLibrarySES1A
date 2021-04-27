08cbcf4a4a6fae60947d4c76ae7bac1fe18129e1
Dylan Jack <dylan4jacks@gmail.com>
2021-04-27 14:29:59 +1000

    convert html to ejs / fixed Register routing / included universal head and Nav partials

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -3,7 +3,7 @@ const registerController = require('../controllers/registerController');
 
 const router = express.Router();
 
-router.get('/register', registerController.register_new_user);
+router.get('/', registerController.register_new_user);
 router.post('/', registerController.register_user);
 
 module.exports = router;
